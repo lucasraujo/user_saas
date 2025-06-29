5c7fa65a6aa79040220ad9f02b5057487397802e
lucas.martins <lucas.martins@cdcbank.com.br>
2025-06-28 22:58:35 -0300

    simplify running the application

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,22 +3,25 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\UserController;
 use App\Controllers\AuthController;
+use App\Controllers\Home;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [Home::class, 'index']);
 
-$routes->addPlaceholder('uuid','[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{8,12}');
 
-$routes->group('auth', function($routes) {
+
+$routes->addPlaceholder('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{8,12}');
+
+$routes->group('api', function ($routes) {
     $routes->post('login', [AuthController::class, 'login']);
-});
 
-$routes->group("user", static function ($routes) {
-    $routes->get("all", [UserController::class, 'getAllUsers'], ['filter' => 'jwt']);
-    $routes->get("(:uuid)", [UserController::class, 'getUser'], ['filter' => 'jwt']);
-    $routes->post("", [UserController::class, 'createUser'], ['filter' => 'jwt']);
-    $routes->patch("(:uuid)", [UserController::class, 'updateUser'], ['filter' => 'jwt']);
-    $routes->delete("(:uuid)", [UserController::class, 'deleteUser'],['filter' => 'jwt']);
+    $routes->group("users", static function ($routes) {
+        $routes->get("", [UserController::class, 'getAllUsers'], ['filter' => 'jwt']);
+        $routes->get("(:uuid)", [UserController::class, 'getUser'], ['filter' => 'jwt']);
+        $routes->post("", [UserController::class, 'createUser'], ['filter' => 'jwt']);
+        $routes->patch("(:uuid)", [UserController::class, 'updateUser'], ['filter' => 'jwt']);
+        $routes->delete("(:uuid)", [UserController::class, 'deleteUser'], ['filter' => 'jwt']);
+    });
 });
